5953621ce2db4cdf2d4f603cadc72ee34eff4d7b
Dan Kirkwood <dangogh@gmail.com>
2021-11-26 15:00:34 -0600

    Day 4

---
+++ 2020/4/day4.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Passport struct {
+	BirthYear      string `validate:"required"`
+	IssueYear      string `validate:"required"`
+	ExpirationYear string `validate:"required"`
+	Height         string `validate:"required"`
+	HairColor      string `validate:"required"`
+	EyeColor       string `validate:"required"`
+	PassportID     string `validate:"required"`
+	CountryID      string
+}
+
+var validate *validator.Validate
+
+func main() {
+	var in io.ReadCloser = os.Stdin
+
+	validate = validator.New()
+	if len(os.Args) > 1 {
+		var err error
+		in, err = os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal("opening " + os.Args[1])
+		}
+		defer in.Close()
+	}
+
+	fileScanner := bufio.NewScanner(in)
+
+	// start with single empty line
+	lines := []string{""}
+	count := 0
+
+	for fileScanner.Scan() {
+		line := strings.TrimSpace(fileScanner.Text())
+		if len(line) == 0 {
+			// start a new line
+			lines = append(lines, "")
+			continue
+		}
+
+		lines[len(lines)-1] += " "+line
+	}
+
+	for _, line := range lines {
+		pass := Passport{}
+		for _, p := range strings.Split(line, " ") {
+			pp := strings.SplitN(p, ":", 2)
+			if len(pp) != 2 {
+				continue
+			}
+			key, val := pp[0], pp[1]
+
+			switch key {
+			case "byr": pass.BirthYear = val
+			case "iyr": pass.IssueYear = val
+			case "eyr": pass.ExpirationYear = val
+			case "hgt": pass.Height = val
+			case "hcl": pass.HairColor = val
+			case "ecl": pass.EyeColor = val
+			case "pid": pass.PassportID = val
+			case "cid": pass.CountryID = val
+			default:
+				log.Fatal("invalid key " + key)
+			}
+		}
+
+		if err := validate.Struct(pass); err != nil {
+			continue
+		}
+		count++
+	}
+
+	fmt.Println("Valid passports: ", count)
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ithub.com/blatimerse/advent
 
 go 1.13
+
+require github.com/go-playground/validator/v10 v10.9.0 // indirect
