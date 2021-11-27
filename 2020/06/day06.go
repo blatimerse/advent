73a510bf17dadf8963f903a8158d70ea79eb5796
Dan Kirkwood <dangogh@gmail.com>
2021-11-27 10:33:47 -0600

    Day 6 part 2

--- 2020/06/day06.go
+++ 2020/06/day06.go
@@ -28,20 +28,35 @@ func main() {
 func loadQuestionData(in io.Reader) {
 	scanner := bufio.NewScanner(in)
 
-	m := make(map[byte]struct{})
+	m := make(map[byte]int)
 	count := 0
+	lines := 0
 	for scanner.Scan() {
 		s := strings.TrimSpace(scanner.Text())
 		if len(s) != 0 {
+			lines++
 			for _, c := range []byte(s) {
-				m[c] = struct{}{}
+				m[c]++
 			}
 			continue
 		}
-		count += len(m)
-		m = make(map[byte]struct{})
+		count += addup(m, lines)
+		lines = 0
+		m = make(map[byte]int)
 	}
 
-	count += len(m)
+	// if last line wasn't blank, need to add it, too..
+	count += addup(m, lines)
 	fmt.Println(count)
+}
+
+func addup(m map[byte]int, lines int) int {
+	count := 0
+	for _, v := range m {
+		if v == lines {
+			count++
+		}
+	}
+	//fmt.Printf("%d of %d lines; m is %v\n", count, lines, m)
+	return count
 }
